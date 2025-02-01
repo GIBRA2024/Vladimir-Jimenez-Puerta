cc3dbebc036418399928029401e21565b9460aee
Mateo Monroy <monroymateo1@gmail.com>
2025-01-31 20:32:44 -0500

    correo

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function descargarContacto1() {
 VERSION:3.0
 FN:Jimenez Puerta Abogados
 TEL: +57 3187324455
+NOTE: facturacion@jimenezpuerta.com
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
